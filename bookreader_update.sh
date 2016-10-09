e7d2b3b7f25087bdc51b13e499f444197ca9fcb4
sakai satoru <endeavor2wako@gmail.com>
2016-10-09 14:50:22 +0900

    sakai

--- bookreader_update.sh
+++ bookreader_update.sh
@@ -1,4 +1,10 @@
 #! /bin/bash
+
+# for LXDE
+# rm -R ~/.local/bin/bookreader-master
+# wget --no-check-certificate https://github.com/sakaisatoru/bookreader/archive/master.zip
+
+# for Xfce4
 rm -R ~/bin/bookreader-master
 wget https://github.com/sakaisatoru/bookreader/archive/master.zip
 unzip master.zip
